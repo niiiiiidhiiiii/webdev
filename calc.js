b7db15807dc9f06f92ec02dd253bf65c8bbbdc19
Nidhi <252024680+niiiiiidhiiiii@users.noreply.github.com>
2026-09-06 11:17:59 +0530

    just a line position change

--- calc.js
+++ calc.js
@@ -1,23 +1,19 @@
-let random = Math.random()
+let random = Math.random();
 
-let a = prompt("Enter first number: ")
-let b = prompt("Enter second number: ")
-let c = prompt("Enter operation: ")
+let a = prompt("Enter first number: ");
+let c = prompt("Enter operation: ");
+let b = prompt("Enter second number: ");
 
 let obj = {
-    "+": "-",
-    "-":"+",
-    "*":"/",
-    "/":"**"
+  "+": "-",
+  "-": "+",
+  "*": "/",
+  "/": "**",
+};
 
-}
-
-if(random > 0.1){
-alert(`The result is  ${eval(`${a}${c}${b}`)}`)
-
-}
-
-else{
-    c =obj[c]
-    alert(`The result is  ${eval(`${a}${c}${b}`)}`)
+if (random > 0.1) {
+  alert(`The result is  ${eval(`${a}${c}${b}`)}`);
+} else {
+  c = obj[c];
+  alert(`The result is  ${eval(`${a}${c}${b}`)}`);
 }
